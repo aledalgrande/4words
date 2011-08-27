fbe0eef056e4ea37f3bfc7b8054ba2bcd85f8674
aledalgrande <aledalgrande@gmail.com>
2011-08-27 13:32:39 +0100

    first page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ FourWords::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  match '/:action', :controller => 'pages', :as => 'static'
+  
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
@@ -49,6 +50,7 @@ FourWords::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
