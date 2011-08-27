f89fd2bf883c8e55692a26f85c5f52d97592dad7
aledalgrande <aledalgrande@gmail.com>
2011-08-27 13:13:27 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FourWords::Application.config.secret_token = 'fcf0ec11a92049338b0771a05169a241360bb70ccb15926ae81be7b645798a6af5760f53d91d088e99e02805855f311b60eb9ac552263be492f3f6490c213b9c'
