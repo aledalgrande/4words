f103f21a97dd6b1f0d6d9cd15da7246f1da0a774
aledalgrande <aledalgrande@gmail.com>
2011-10-15 13:57:12 +0100

    about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   end
   
   def about
+    @personas = [{:img => 'ale', :name => 'Alessandro Dal Grande', :role => 'Web Developer'}, {:img => 'ant', :name => 'Anthony Wales', :role => 'Project Coordinator'}, {:img => 'dan', :name => 'Daniel Lewis', :role => 'Graphic Designer'}, {:img => 'daz', :name => 'Darren Pinner', :role => 'PR Director'}].shuffle
   end
   
 end
