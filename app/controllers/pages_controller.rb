8a65cf99799bcfd51f92e37246b288434e112c85
aledalgrande <aledalgrande@gmail.com>
2011-10-15 14:22:37 +0100

    my description

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,28 @@ class PagesController < ApplicationController
   end
   
   def about
-    @personas = [{:img => 'ale', :name => 'Alessandro Dal Grande', :role => 'Web Developer'}, {:img => 'ant', :name => 'Anthony Wales', :role => 'Project Coordinator'}, {:img => 'dan', :name => 'Daniel Lewis', :role => 'Graphic Designer'}, {:img => 'daz', :name => 'Darren Pinner', :role => 'PR Director'}].shuffle
+    @personas = [
+      {
+        :img => 'ale',
+        :name => 'Alessandro Dal Grande',
+        :role => 'Web Developer',
+        :desc => "His angel face will diddle you. Merciless entity from Hell, sometimes he's struck by a strange energy and becomes  temporarily a kind person that wants to help the most needful. Some say he can see through your webcam by looking at his wristwatch and that he is the inspiration for new fashion trends in Jakarta. All we know, he's called Alessandro."
+      }, {
+        :img => 'ant',
+        :name => 'Anthony Wales',
+        :role => 'Project Coordinator',
+        :desc => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      }, {
+        :img => 'dan',
+        :name => 'Daniel Lewis',
+        :role => 'Graphic Designer',
+        :desc => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      }, {
+        :img => 'daz',
+        :name => 'Darren Pinner',
+        :role => 'PR Director',
+        :desc => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      }].shuffle
   end
   
 end
